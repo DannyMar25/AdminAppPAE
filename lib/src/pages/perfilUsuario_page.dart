5e7b56a71eba07df330bb521bcf853b02d8f5340
DannyMar25 <danny250393@gmail.com>
2023-01-25 21:45:12 -0500

    perfil

--- lib/src/pages/perfilUsuario_page.dart
+++ lib/src/pages/perfilUsuario_page.dart
@@ -48,247 +48,252 @@ class _PerfilUsuarioPageState extends State<PerfilUsuarioPage> {
       ),
       body: Form(
         key: formKey,
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 5)),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/admin.jpg'),
-              radius: 50,
-              foregroundColor: Colors.black,
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 10)),
-            Text(
-              'Hola ' + nombreS,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 20)),
-            Center(
-              child: SizedBox(
-                width: 350,
-                child: TextFormField(
-                  obscureText: !_passwordVisible,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  validator: (val) {
-                    if (val!.isEmpty) return 'Vacío';
-                    if (val.length < 8)
-                      return 'Contraseña mayor o igual a 8 caracteres.';
-                    return null;
-                  },
-                  onChanged: (s) {
-                    setState(() {
-                      passActual = s;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        // Based on passwordVisible state choose the icon
-                        _passwordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 5)),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/admin.jpg'),
+                radius: 50,
+                foregroundColor: Colors.black,
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 15)),
+              Text(
+                'Hola ' + nombreS,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              Center(
+                child: SizedBox(
+                  width: 320, //350
+                  child: TextFormField(
+                    obscureText: !_passwordVisible,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    validator: (val) {
+                      if (val!.isEmpty) return 'Vacío';
+                      if (val.length < 8)
+                        return 'Contraseña mayor o igual a 8 caracteres.';
+                      return null;
+                    },
+                    onChanged: (s) {
+                      setState(() {
+                        passActual = s;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // Based on passwordVisible state choose the icon
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.green,
+                        ),
+                        onPressed: () {
+                          // Update the state i.e. toogle the state of passwordVisible variable
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+
+                      focusColor: Colors.white,
+                      //add prefix icon
+                      prefixIcon: Icon(
+                        Icons.lock,
                         color: Colors.green,
                       ),
-                      onPressed: () {
-                        // Update the state i.e. toogle the state of passwordVisible variable
-                        setState(() {
-                          _passwordVisible = !_passwordVisible;
-                        });
-                      },
-                    ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
 
-                    focusColor: Colors.white,
-                    //add prefix icon
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.green,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.green, width: 1.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    fillColor: Colors.green,
-                    labelText: 'Contraseña actual',
-                    //lable style
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: "verdana_regular",
-                      fontWeight: FontWeight.w400,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.green, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fillColor: Colors.green,
+                      labelText: 'Contraseña actual',
+                      //lable style
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: "verdana_regular",
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 20)),
-            Center(
-              child: SizedBox(
-                width: 350,
-                child: TextFormField(
-                  controller: passNew,
-                  obscureText: !_passwordVisible1,
-                  style: TextStyle(
-                    fontSize: 20,
-                    //color: Colors.blue,
-                    //fontWeight: FontWeight.w600,
-                  ),
-                  validator: (val) {
-                    if (val!.isEmpty) return 'Vacío';
-                    if (val.length < 8)
-                      return 'Contraseña mayor o igual a 8 caracteres.';
-                    return null;
-                  },
-                  onSaved: (s) {
-                    setState(() {
-                      passNew = s as TextEditingController;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        // Based on passwordVisible state choose the icon
-                        _passwordVisible1
-                            ? Icons.visibility
-                            : Icons.visibility_off,
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              Center(
+                child: SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    controller: passNew,
+                    obscureText: !_passwordVisible1,
+                    style: TextStyle(
+                      fontSize: 20,
+                      //color: Colors.blue,
+                      //fontWeight: FontWeight.w600,
+                    ),
+                    validator: (val) {
+                      if (val!.isEmpty) return 'Vacío';
+                      if (val.length < 8)
+                        return 'Contraseña mayor o igual a 8 caracteres.';
+                      return null;
+                    },
+                    onSaved: (s) {
+                      setState(() {
+                        passNew = s as TextEditingController;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // Based on passwordVisible state choose the icon
+                          _passwordVisible1
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.green,
+                        ),
+                        onPressed: () {
+                          // Update the state i.e. toogle the state of passwordVisible variable
+                          setState(() {
+                            _passwordVisible1 = !_passwordVisible1;
+                          });
+                        },
+                      ),
+                      focusColor: Colors.white,
+                      //add prefix icon
+                      prefixIcon: Icon(
+                        Icons.lock,
                         color: Colors.green,
                       ),
-                      onPressed: () {
-                        // Update the state i.e. toogle the state of passwordVisible variable
-                        setState(() {
-                          _passwordVisible1 = !_passwordVisible1;
-                        });
-                      },
-                    ),
-                    focusColor: Colors.white,
-                    //add prefix icon
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.green,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
 
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.green, width: 1.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    fillColor: Colors.green,
-                    labelText: 'Nueva contraseña',
-                    //lable style
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: "verdana_regular",
-                      fontWeight: FontWeight.w400,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.green, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fillColor: Colors.green,
+                      labelText: 'Nueva contraseña',
+                      //lable style
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: "verdana_regular",
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 20)),
-            Center(
-              child: SizedBox(
-                width: 350,
-                child: TextFormField(
-                  controller: passConfirm,
-                  obscureText: !_passwordVisible2,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  validator: (val) {
-                    if (val!.isEmpty) return 'Vacío';
-                    if (val.length < 8)
-                      return 'Contraseña mayor o igual a 8 caracteres.';
-                    if (val != passNew.text) return 'No coincide';
-                    return null;
-                  },
-                  onSaved: (s) {
-                    setState(() {
-                      passConfirm = s as TextEditingController;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        // Based on passwordVisible state choose the icon
-                        _passwordVisible2
-                            ? Icons.visibility
-                            : Icons.visibility_off,
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              Center(
+                child: SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    controller: passConfirm,
+                    obscureText: !_passwordVisible2,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    validator: (val) {
+                      if (val!.isEmpty) return 'Vacío';
+                      if (val.length < 8)
+                        return 'Contraseña mayor o igual a 8 caracteres.';
+                      if (val != passNew.text) return 'No coincide';
+                      return null;
+                    },
+                    onSaved: (s) {
+                      setState(() {
+                        passConfirm = s as TextEditingController;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // Based on passwordVisible state choose the icon
+                          _passwordVisible2
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.green,
+                        ),
+                        onPressed: () {
+                          // Update the state i.e. toogle the state of passwordVisible variable
+                          setState(() {
+                            _passwordVisible2 = !_passwordVisible2;
+                          });
+                        },
+                      ),
+                      focusColor: Colors.white,
+                      //add prefix icon
+                      prefixIcon: Icon(
+                        Icons.lock,
                         color: Colors.green,
                       ),
-                      onPressed: () {
-                        // Update the state i.e. toogle the state of passwordVisible variable
-                        setState(() {
-                          _passwordVisible2 = !_passwordVisible2;
-                        });
-                      },
-                    ),
-                    focusColor: Colors.white,
-                    //add prefix icon
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.green,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
 
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.green, width: 1.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    fillColor: Colors.green,
-                    labelText: 'Confirmar contraseña',
-                    //lable style
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: "verdana_regular",
-                      fontWeight: FontWeight.w400,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.green, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fillColor: Colors.green,
+                      labelText: 'Confirmar contraseña',
+                      //lable style
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: "verdana_regular",
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 20)),
-            ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith(
-                    (Set<MaterialState> states) {
-                  return Colors.green;
-                }),
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    return Colors.green;
+                  }),
+                ),
+                label: Text('Actualizar'),
+                icon: Icon(Icons.update),
+                autofocus: true,
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    _changePassword(passActual, passNew.text);
+                    SnackBar(
+                      content: Text('Información ingresada correctamente.'),
+                    );
+                    mostrarAlertaOk1(
+                        context,
+                        'Contraseña actualizada con éxito.',
+                        'login',
+                        'Información');
+                  } else {
+                    mostrarAlerta(context,
+                        'Asegúrate de haber ingresado correctamente la contraseña.');
+                  }
+                },
               ),
-              label: Text('Actualizar'),
-              icon: Icon(Icons.update),
-              autofocus: true,
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  _changePassword(passActual, passNew.text);
-                  SnackBar(
-                    content: Text('Información ingresada correctamente.'),
-                  );
-                  mostrarAlertaOk1(context, 'Contraseña actualizada con éxito.',
-                      'login', 'Información');
-                } else {
-                  mostrarAlerta(context,
-                      'Asegúrate de haber ingresado correctamente la contraseña.');
-                }
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
